6b20eb37fc2143911b4b677afa11bc21ec92477c
Guilherme F <guilherme.fre073@gmail.com>
2021-02-18 08:05:21 -0300

    Realizado continuação dos cursos.

--- ruby3_avancado/main.rb
+++ ruby3_avancado/main.rb
@@ -5,5 +5,5 @@ inicia_fogefoge
 
 # Continuar na aula
 # Ruby III : Mais Lógica de programação em um novo jogo
-# 04. Conceitos de Classes e Métodos
-#   01 - Agrupamento
+# 05. Usando recursao para solucionar problemas
+#  03 - Resolvendo bugs
