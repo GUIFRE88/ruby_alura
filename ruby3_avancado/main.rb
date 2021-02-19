f9d930fd21cb3099b563ec524d2fc7673f34a233
Guilherme F <guilherme.fre073@gmail.com>
2021-02-19 07:18:23 -0300

    FInalizado curso.

--- ruby3_avancado/main.rb
+++ ruby3_avancado/main.rb
@@ -1,9 +1,3 @@
 require_relative 'fogefoge'
 
 inicia_fogefoge
-
-
-# Continuar na aula
-# Ruby III : Mais Lógica de programação em um novo jogo
-# 05. Usando recursao para solucionar problemas
-#  03 - Resolvendo bugs
